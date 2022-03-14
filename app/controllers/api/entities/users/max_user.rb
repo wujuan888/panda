0d7f2b862368d3939c3032c7e986fd0b5083b902
wujuan <991582714@qq.com>
2022-03-14 22:01:29 +0800

    user uuid

--- app/controllers/api/entities/users/max_user.rb
+++ app/controllers/api/entities/users/max_user.rb
@@ -4,7 +4,7 @@ module Api
   module Entities
     module Users
       class MaxUser < Grape::Entity
-        expose :id, :real_name, :head_url, :phone
+        expose :id, :uuid, :real_name, :head_url, :phone
         expose :role
         expose :role_name do |instance, _|
           ::User.roles[instance.role][0]
--- app/controllers/api/entities/users/min_user.rb
+++ app/controllers/api/entities/users/min_user.rb
@@ -4,7 +4,7 @@ module Api
   module Entities
     module Users
       class MaxUser < Grape::Entity
-        expose :id, :real_name
+        expose :id, uuid, :real_name
         expose :role_name do |instance, _|
           ::User.roles[instance.role][0]
         end
