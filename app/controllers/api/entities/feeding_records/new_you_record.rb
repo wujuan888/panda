259c3d7e50d317b744f60aa3b4a3f5b8ab8f7c0b
wujuan <991582714@qq.com>
2023-02-17 10:51:54 +0800

    panda

--- app/controllers/api/entities/feeding_records/new_you_record.rb
+++ app/controllers/api/entities/feeding_records/new_you_record.rb
@@ -4,11 +4,35 @@ module Api
   module Entities
     module FeedingRecords
       class NewYouRecord < Grape::Entity
+        expose :id, :weight, :panda_id, :remark, :user_id
+        expose :created_at, format_with: :localtime
+        expose :time, format_with: :only_date
         expose :panda do |instance, option|
           Entities::Pandas::BasePanda.represent ::Panda.find(option[:panda_id])
         end
+        expose :milk_record do |instance, _|
+          Entities::MilkRecord::Data.represent instance.milk_record
+        end
+        expose :temperature_record do |instance, _|
+          Entities::TemperatureRecord::Data.represent instance.temperature_record
+        end
+        expose :drug_records do |instance, _|
+          Entities::DrugRecord::Data.represent instance.drug_records
+        end
+        expose :poop_record do |instance, _|
+          Entities::PoopRecord::Data.represent instance.poop_record
+        end
+        expose :com_evaluation_record do |instance, _|
+          Entities::ComEvaluationRecord::Data.represent instance.com_evaluation_record
+        end
+        expose :states_records do |instance, _|
+          Entities::StatesRecord::MaxData.represent instance.states_records
+        end
         expose :panda_states_records do |instance, option|
           Entities::StatesRecord::Data.represent ::StatesRecord.with_panda_current(option[:panda_id])
+        end
+        expose :image_list do |instance, _|
+          Entities::Attachment.represent instance.attachments
         end
         expose :drug_list do |_|
           Entities::Drug::MaxData.represent ::Drug.all
