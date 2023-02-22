2602114a05da8c8e36ecaaafac8846f107c184cd
wujuan <991582714@qq.com>
2023-02-22 13:55:22 +0800

    d

--- app/controllers/api/entities/dormitories/max_dormitory.rb
+++ app/controllers/api/entities/dormitories/max_dormitory.rb
@@ -6,7 +6,7 @@ module Api
       class MaxDormitory < Grape::Entity
         expose :id, :name, :place_id, :district_id, :panda_count
         expose :position do |instance, _|
-          "#{instance.place.name}-#{instance.district.name}"
+          "#{instance.place&.name}-#{instance.district&.name}"
         end
         expose :panda_names do |instance, _|
           instance.pandas.pluck('name')&.join('、')
